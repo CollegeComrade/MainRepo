f336b6781f82026cd85f9f0414aaf558ca25a330
Srujan <srujan@Srujans-MacBook-Air.local>
2022-10-22 19:45:44 +0530

    added component and screen

---
+++ lib/screens/UikHomePage.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+      ),
+      body:SingleChildScrollView(
+        child:Container(
+          padding:EdgeInsets.all(8),
+          child:Column(children: [
+            Text("hello college comrade")
+          ],)
+
+
+        )
+      )
+    );
+  }
+}
