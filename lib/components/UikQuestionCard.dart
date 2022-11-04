b7df604c87807ac93f18a9b0ca598aab1150b80b
Srujan <srujan@Srujans-MacBook-Air.local>
2022-11-05 00:02:55 +0530

    added career component

---
+++ lib/components/UikQuestionCard.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class QuestionCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text("hai")
+   
+    );
+  }
+}
